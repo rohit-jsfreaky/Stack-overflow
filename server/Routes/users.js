6653ec2a2394829d3c0b43502880426e1ce79c72
Rohit <rohitkashyapmrt@gmail.com>
2024-05-26 21:05:05 +0530

    patch

--- server/Routes/users.js
+++ server/Routes/users.js
@@ -11,7 +11,7 @@ router.get('/getAllUsers', getAllUsers);
 router.patch('/update/:id', updateProfile);
 
 router.post('/forgot-password', forgotPassword);
-router.patch('/reset-password/:id/:token', resetPassword);
+router.get('/reset-password/:id/:token', resetPassword);
 router.post('/email-send-otp',sendOtp);
 router.get('/email-get-otp',getOtp);
 router.post('/mobile-send-otp',sendMobileOtp);  
