73f5593e4a7c06add7f3dcb69fdb91ce38e2d2f9
Rohit <rohitkashyapmrt@gmail.com>
2024-05-26 19:01:41 +0530

    second

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({ baseURL: 'http://localhost:5000/' })
+const API = axios.create({ baseURL: 'https://stack-overflow-gamma-orcin.vercel.app/' })
 
 
 export const logIn = (authdata) => API.post('/user/login', authdata);
