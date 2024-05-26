f7ff7556b36b066b9cd018a5c541550f3fa59d06
Rohit <rohitkashyapmrt@gmail.com>
2024-05-26 18:59:52 +0530

    first

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({ baseURL: 'https://stack-vercel.vercel.app/' })
+const API = axios.create({ baseURL: 'http://localhost:5000/' })
 
 
 export const logIn = (authdata) => API.post('/user/login', authdata);
