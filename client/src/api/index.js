34da94203e8d7929779a661d76c3a33809bc59da
Rohit <rohitkashyapmrt@gmail.com>
2024-05-27 10:35:59 +0530

    changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({ baseURL: 'https://stack-overflow-delta-lemon.vercel.app/' })
+const API = axios.create({ baseURL: 'https://stack-overflow-z5qu.vercel.app/' })
 
 
 export const logIn = (authdata) => API.post('/user/login', authdata);
